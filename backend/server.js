b8454daa05ab970ccc61ea7dc59148db0b8b06d0
manminder singh <bewithme2407@gmail.com>
2024-04-24 14:16:25 +0530

    user routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import authRoute from "./routes/authRote.js"
 import dotenv from 'dotenv'
 import { connect } from "./models/connection.js";
+import { MONGO } from "../config.js"
+import cookieParser from "cookie-parser";
 dotenv.config()
 const app = express();
+app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoute)
-console.log(process.env.MONGO_URI);
+console.log(MONGO);
 app.listen(3000, () => {
   console.log("Example app listening at http://localhost:3000");
   connect();
